3c4c80c6c5a77079e5f4d7a286234894dc1ec1c0
kwak <byunghokwak5497@gmail.com>
2017-05-02 18:01:12 +0900

    init1

---
+++ Algorithm.java
@@ -0,0 +1,10 @@
+import java.awt.Point;
+import java.util.*;
+import java.util.Arrays;
+
+public class Algorithm {  
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
